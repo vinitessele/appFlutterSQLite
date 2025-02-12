8d613463ca5d79a5cd359cbc0ec4e80cac2906e0
vinitessele <vinicius_tessele@hotmail.com>
2025-02-11 21:48:11 -0300

    first commit

---
+++ lib/database_helper.dart
@@ -0,0 +1,67 @@
+import 'package:sqflite/sqflite.dart';
+import 'package:path/path.dart';
+import 'contato.dart';
+
+class DatabaseHelper {
+  static final DatabaseHelper instance = DatabaseHelper._init();
+  static Database? _database;
+
+  DatabaseHelper._init();
+
+  Future<Database> get database async {
+    if (_database != null) return _database!;
+    _database = await _initDB('contacts.db');
+    return _database!;
+  }
+
+  Future<Database> _initDB(String fileName) async {
+    final dbPath = await getDatabasesPath();
+    final path = join(dbPath, fileName);
+
+    return await openDatabase(
+      path,
+      version: 1,
+      onCreate: _createDB,
+    );
+  }
+
+  Future<void> _createDB(Database db, int version) async {
+    await db.execute('''
+      CREATE TABLE contacts (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name TEXT NOT NULL,
+        phone TEXT NOT NULL
+      )
+    ''');
+  }
+
+  Future<int> insertContact(Contact contact) async {
+    final db = await database;
+    return await db.insert('contacts', contact.toMap());
+  }
+
+  Future<List<Contact>> getContacts() async {
+    final db = await database;
+    final result = await db.query('contacts');
+    return result.map((map) => Contact.fromMap(map)).toList();
+  }
+
+  Future<int> updateContact(Contact contact) async {
+    final db = await database;
+    return await db.update(
+      'contacts',
+      contact.toMap(),
+      where: 'id = ?',
+      whereArgs: [contact.id],
+    );
+  }
+
+  Future<int> deleteContact(int id) async {
+    final db = await database;
+    return await db.delete(
+      'contacts',
+      where: 'id = ?',
+      whereArgs: [id],
+    );
+  }
+}
